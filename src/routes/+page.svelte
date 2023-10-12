cecb1a5b32ef535c6322c26ad4bfb383edcb435d
Juan Ramirez <juanpram2000@gmail.com>
2023-10-11 22:17:28 -0700

    Done building the component

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<div class="w-screen h-screen text-lg md:flex md:flex-col md:items-center md:justify-center">
+    <div class="flex flex-col md:flex-row shadow-2xl shadow-[#ebf1ff] w-screen h-screen md:w-[44rem] md:h-[32rem] md rounded-3xl ">
+        <div class="flex flex-col w-full h-4/12 md:h-full md:w-1/2 text-white bg-gradient-to-b from-[#7857ff] to-[#2e2be9] md:rounded-3xl rounded-b-3xl p-10 items-center text-center gap-6">
+            <p class="text-xl font-bold text-white/70">Your Result</p>
+            <div class="h-40 w-40 md:h-48 md:w-48 rounded-full bg-gradient-to-b from-[#4e21ca] to-[#2421ca] flex flex-col items-center justify-center">
+                <h2 class="text-6xl font-extrabold md:text-7xl">76</h2>
+                <p class="text-white/60">of 100</p>
+            </div>
+            <h2 class="text-4xl">Great</h2>
+            <p class="text-white/60">You scored higher than 65% of the people who have taken these tests.</p>
+        </div>
+        <div class="flex flex-col w-full gap-8 p-10 h-8/12 md:h-full md:w-1/2">
+            <h2 class="text-2xl font-extrabold">Summary</h2>
+            <ul class="flex flex-col gap-4">
+                <li class="w-full bg-[#ff5757]/10 p-3 rounded-lg flex flex-row gap-1">
+                    <img src={'./assets/images/icon-reaction.svg'} alt="reaction">
+                    <p class="text-[#ff5757] font-extrabold grow">Reaction</p>
+                    <p class="font-semibold text-black/40"><strong class="text-black">80</strong> / 100</p>
+                </li>
+                <li class="w-full bg-[#ffb01f]/10 p-3 rounded-lg flex flex-row gap-1">
+                    <img src={'./assets/images/icon-memory.svg'} alt="memory">
+                    <p class="text-[#ffb01f] font-extrabold grow">Memory</p>
+                    <p class="font-semibold text-black/40"><strong class="text-black">92</strong> / 100</p>
+                </li>
+                <li class="w-full bg-[#00bd91]/10 p-3 rounded-lg flex flex-row gap-1">
+                    <img src={'./assets/images/icon-verbal.svg'} alt="verbal">
+                    <p class="text-[#00bd91] font-extrabold grow ">Verbal</p>
+                    <p class="font-semibold text-black/40"><strong class="text-black">61</strong> / 100</p>
+                </li>
+                <li class="w-full bg-[#1125d4]/10 p-3 rounded-lg flex flex-row gap-1">
+                    <img src={'./assets/images/icon-visual.svg'} alt="visual">
+                    <p class="text-[#1125d4] font-extrabold grow">Visual</p>
+                    <p class="font-semibold text-black/40"><strong class="text-black">72</strong> / 100</p>
+                </li>
+            </ul>
+            <button class="w-full bg-[#303b5a] py-3 hover:bg-gradient-to-b hover:from-[#7857ff] hover:to-[#2e2be9] rounded-3xl text-white/80 font-bold">Continue</button>
+        </div>
+    </div>
+</div>
